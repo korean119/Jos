95e55f75c26ebcd4960940c84702a478fe47834c
신준오 <@DESKTOP-F3B2TKP>
2023-02-20 01:05:44 +0900

    Testrail / TestNg / POM 적용 - 근데 너무 어거지로 껴놓아서 리팩토링 필요할듯

--- SeleniumJO/src/test/java/Script/_1_Login.java
+++ SeleniumJO/src/test/java/Script/_1_Login.java
@@ -1,20 +1,37 @@
 package Script;
 
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.simple.JSONObject;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-import Testrail_listeners.TestNgTestRailListener;
-import Util.UseAsTestRailId;
+import Listener.ListenerTest;
+import Testrail_client.APIClient;
+import Testrail_client.Testrails.TestRails;
 
-@Listeners(TestNgTestRailListener.class)
+
+
+@Listeners(ListenerTest.class)
 public class _1_Login extends _0_BaseTest {
+	
+	public static final int TEST_CASE_PASSED_STATUS = 1;
+	public static final int TEST_CASE_FAILED_STATUS = 5;
 
+	@TestRails(id = "7927")
 	@Test
-	@UseAsTestRailId(testRailId = 26419)
+
 	public void login() throws InterruptedException {
 
 		objLoginPage.Login("joshin1@getnada.com", "tlswnsdh2@@");
 
 	}
-
 }
